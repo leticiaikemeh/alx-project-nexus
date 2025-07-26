88601dec2a506b1003b6b78498c82e96344d2295
root <root@DESKTOP-F8D6VBB.localdomain>
2025-07-26 17:33:55 +0100

    config docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,12 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z db 5432; do
-  sleep 0.1
-done
+# Wait for PostgreSQL to be ready
+/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Postgres is up"
 
-echo "PostgreSQL started"
+echo "Running migrations..."
+python manage.py migrate
 
+echo "Starting server..."
 exec "$@"
+
